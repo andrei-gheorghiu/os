4de30f612d68bb7683107cf4747a16a3d8256e52
andrei <andrei.gheorghiu@evolution.ro>
2016-11-02 08:06:32 +0200

    should be enoug

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,94 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
+
     grunt.initConfig({
-        autoprefixer: {
-            dist: {
-                files: {
-                    'assets/css/open.sky.css': 'source/open.sky.css'
-                }
-            },
+        pkg: grunt.file.readJSON('package.json'),
+        postcss: {
             options: {
-                browsers:['> 0.01%']
+                processors: [
+                    require('autoprefixer')({browsers: ['> 0.01%']}),
+                    require('cssnext')(),
+                    require('precss')()
+                ]
+            },
+            dist: {
+                src: 'source/open.sky.css',
+                dest: 'assets/css/open.sky.css'
             }
+
         },
         watch: {
             styles: {
                 files: ['source/open.sky.css'],
-                tasks: ['autoprefixer']
+                tasks: ['postcss']
+            }
+        },
+        modernizr: {
+            dist: {
+                // Avoid unnecessary builds (see Caching section below)
+                "cache" : true,
+
+                // Path to the build you're using for development.
+                "devFile" : false,
+
+                // Path to save out the built file
+                "dest" : "/assets/mdrnzr/modernizr.js",
+
+                // Based on default settings on http://modernizr.com/download/
+                "options" : [
+                    "setClasses",
+                    "addTest",
+                    "html5printshiv",
+                    "testProp",
+                    "fnBind"
+                ],
+
+                // By default, source is uglified before saving
+                "uglify" : true,
+
+                // Define any tests you want to explicitly include
+                "tests" : [],
+
+                // Useful for excluding any tests that this tool will match
+                // e.g. you use .notification class for notification elements,
+                // but don’t want the test for Notification API
+                "excludeTests": [],
+
+                // By default, will crawl your project for references to Modernizr tests
+                // Set to false to disable
+                "crawl" : true,
+
+                // Set to true to pass in buffers via the "files" parameter below
+                "useBuffers" : false,
+
+                // By default, this task will crawl all *.js, *.css, *.scss files.
+                "files" : {
+                    "src": [
+                        "*[^(g|G)runt(file)?].{js,css,scss}",
+                        "**[^node_modules]/**/*.{js,css,scss}",
+                        "!lib/**/*"
+                    ]
+                },
+
+                // Have custom Modernizr tests? Add them here.
+                "customTests" : []
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'assets/js/open.sky.js',
+                dest: 'assets/js/open.sky.min.js'
             }
         }
+
     });
-    grunt.loadNpmTasks('grunt-autoprefixer');
+
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks("grunt-modernizr");
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    //grunt.registerTask('default', ['uglify']);
 };
--- assets/js/open.sky.js
+++ assets/js/open.sky.js
@@ -17,144 +17,153 @@ jQuery.noConflict();
             duration: 1000
         });
     });
-    /*window.refreshSlicks = function(){
-        $('.partnersCarousel').slick('refresh');
-        $('.topSlider').slick('refresh');
-        $('.sectorsCarousel').slick('refresh');
-        $('.clientsCarousel').slick('refresh');
+    window.slicks = {
+        init : function() {
+            window.slicks.clients();
+            window.slicks.partners();
+            $('.topSlider').slick({
+                dots: false,
+                autoplay: true,
+                autoplaySpeed: 6000,
+                infinite: true,
+                speed: 1000,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                cssEase: 'ease-in-out'
+            });
+            $('.sectorsCarousel').slick({
+                dots: false,
+                autoplay: true,
+                autoplaySpeed: 4200,
+                infinite: true,
+                speed: 600,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                cssEase:'ease-in-out'
+            }).on('afterChange', function(e){
+                var labels = $('.sectorsSlider label');
+                labels.removeClass('active');
+                labels.eq($(this).slick('slickCurrentSlide')).addClass('active');
+                document.getElementById('rangeInput').value = $(this).slick('slickCurrentSlide') + 1;
+            });
+        },
+        clients:function(){
+            $('.clientsCarousel').slick({
+                dots: false,
+                autoplay: false,
+                infinite: true,
+                speed: 600,
+                slidesToShow: 5,
+                slidesToScroll: 5,
+                cssEase:'ease-in-out',
+                responsive: [
+                    {
+                        breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 4,
+                            slidesToScroll: 4
+                        }
+                    },
+                    {
+                        breakpoint: 980,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3
+                        }
+                    },
+                    {
+                        breakpoint: 780,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: "unslick"
+                    }
+                ],
+                prevArrow: '<a href="#" data-role="none" class="carousel-prev" aria-label="Previous" tabindex="0" role="button">' +
+                '<svg version="1.1" id="leftArrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" ' +
+                'width="50px" height="50px" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">' +
+                '<path d="M50,25C50,11.193,38.807,0,25,0S0,11.193,0,25s11.193,25,25,25S50,38.807,50,25z M13.946,24.252L29.144,9.094 ' +
+                'c0.217-0.217,0.467-0.325,0.75-0.325c0.282,0,0.532,0.108,0.75,0.325l1.631,1.626c0.217,0.216,0.326,0.466,0.326,0.748 ' +
+                'c0,0.282-0.109,0.531-0.326,0.748L19.458,25l12.817,12.784c0.217,0.217,0.326,0.466,0.326,0.748c0,0.282-0.109,0.531-0.326,0.748 ' +
+                'l-1.631,1.626c-0.218,0.217-0.468,0.325-0.75,0.325c-0.283,0-0.533-0.109-0.75-0.325L13.946,25.748 ' +
+                'C13.728,25.531,13.62,25.282,13.62,25C13.62,24.718,13.728,24.468,13.946,24.252z" fill="currentColor" />' +
+                '</svg></button>',
+                nextArrow: '<a href="#" data-role="none" class="carousel-next" aria-label="Next" tabindex="0" role="button">' +
+                '<svg version="1.1" id="rightArrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" ' +
+                'width="50px" height="50px" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">' +
+                '<path d="M0,25c0,13.807,11.193,25,25,25s25-11.193,25-25S38.807,0,25,0S0,11.193,0,25z M36.054,25.748L20.856,40.906 ' +
+                'c-0.217,0.217-0.467,0.325-0.75,0.325c-0.282,0-0.532-0.108-0.75-0.325l-1.631-1.626c-0.217-0.216-0.326-0.466-0.326-0.748 ' +
+                'c0-0.282,0.109-0.531,0.326-0.748L30.542,25L17.725,12.216c-0.217-0.217-0.326-0.466-0.326-0.748c0-0.282,0.109-0.531,0.326-0.748 ' +
+                'l1.631-1.626c0.218-0.217,0.468-0.325,0.75-0.325c0.283,0,0.533,0.109,0.75,0.325l15.198,15.158 ' +
+                'c0.218,0.217,0.326,0.467,0.326,0.748C36.38,25.282,36.272,25.532,36.054,25.748z" fill="currentColor" />' +
+                '</svg></button>',
+            });
+        },
+        partners:function(){
+            $('.partnersCarousel').slick({
+                dots: false,
+                arrows: false,
+                speed: 600,
+                slidesToShow: 7,
+                slidesToScroll: 7,
+                cssEase: 'ease-in-out',
+                responsive: [
+                    {
+                        breakpoint: 9999,
+                        settings: "unslick"
+                    },
+                    {
+                        breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 5,
+                            slidesToScroll: 5
+                        }
+                    },
+                    {
+                        breakpoint: 980,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3
+                        }
+                    },
+                    {
+                        breakpoint: 780,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: "unslick"
+                    }
+                ]
+            });
+        }
     };
-    $(window).on('resize', function(){
-        window.refreshSlicks();
-    });*/
-    $('.partnersCarousel').slick({
-        dots: false,
-        arrows: false,
-        speed: 600,
-        slidesToShow: 7,
-        slidesToScroll: 7,
-        cssEase: 'ease-in-out',
-        responsive: [
-            {
-                breakpoint: 2000,
-                settings: "unslick"
-            },
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 5
-                }
-            },
-            {
-                breakpoint: 980,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3
-                }
-            },
-            {
-                breakpoint: 780,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: "unslick"
-            },
-        ]
-    });
+    window.slicks.init();
     $(".menuToggle").on( "click tap", function() {
         $('.touchMenu').toggleClass( "active" );
     });
-
-    $('.topSlider').slick({
-        dots: false,
-        autoplay: true,
-        autoplaySpeed: 6000,
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        cssEase: 'ease-in-out'
-    });
-
-    $('.sectorsCarousel').slick({
-        dots: false,
-        autoplay: true,
-        autoplaySpeed: 4200,
-        infinite: true,
-        speed: 600,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        cssEase:'ease-in-out'
-    }).on('afterChange', function(e){
-        var labels = $('.sectorsSlider label');
-        labels.removeClass('active');
-        labels.eq($(this).slick('slickCurrentSlide')).addClass('active');
-        document.getElementById('rangeInput').value = $(this).slick('slickCurrentSlide') + 1;
-    });
-
-    $('.clientsCarousel').slick({
-        dots: false,
-        autoplay: false,
-        infinite: true,
-        speed: 600,
-        slidesToShow: 5,
-        slidesToScroll: 5,
-        cssEase:'ease-in-out',
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 4
-                }
-            },
-            {
-                breakpoint: 980,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3
-                }
-            },
-            {
-                breakpoint: 780,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 375,
-                settings: "unslick"
-            }
-        ],
-        prevArrow: '<a href="#" data-role="none" class="carousel-prev" aria-label="Previous" tabindex="0" role="button">' +
-        '<svg version="1.1" id="leftArrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" ' +
-        'width="50px" height="50px" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">' +
-        '<path d="M50,25C50,11.193,38.807,0,25,0S0,11.193,0,25s11.193,25,25,25S50,38.807,50,25z M13.946,24.252L29.144,9.094 ' +
-        'c0.217-0.217,0.467-0.325,0.75-0.325c0.282,0,0.532,0.108,0.75,0.325l1.631,1.626c0.217,0.216,0.326,0.466,0.326,0.748 ' +
-        'c0,0.282-0.109,0.531-0.326,0.748L19.458,25l12.817,12.784c0.217,0.217,0.326,0.466,0.326,0.748c0,0.282-0.109,0.531-0.326,0.748 ' +
-        'l-1.631,1.626c-0.218,0.217-0.468,0.325-0.75,0.325c-0.283,0-0.533-0.109-0.75-0.325L13.946,25.748 ' +
-        'C13.728,25.531,13.62,25.282,13.62,25C13.62,24.718,13.728,24.468,13.946,24.252z" fill="currentColor" />' +
-        '</svg></button>',
-        nextArrow: '<a href="#" data-role="none" class="carousel-next" aria-label="Next" tabindex="0" role="button">' +
-            '<svg version="1.1" id="rightArrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" ' +
-        'width="50px" height="50px" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">' +
-        '<path d="M0,25c0,13.807,11.193,25,25,25s25-11.193,25-25S38.807,0,25,0S0,11.193,0,25z M36.054,25.748L20.856,40.906 ' +
-        'c-0.217,0.217-0.467,0.325-0.75,0.325c-0.282,0-0.532-0.108-0.75-0.325l-1.631-1.626c-0.217-0.216-0.326-0.466-0.326-0.748 ' +
-        'c0-0.282,0.109-0.531,0.326-0.748L30.542,25L17.725,12.216c-0.217-0.217-0.326-0.466-0.326-0.748c0-0.282,0.109-0.531,0.326-0.748 ' +
-        'l1.631-1.626c0.218-0.217,0.468-0.325,0.75-0.325c0.283,0,0.533,0.109,0.75,0.325l15.198,15.158 ' +
-        'c0.218,0.217,0.326,0.467,0.326,0.748C36.38,25.282,36.272,25.532,36.054,25.748z" fill="currentColor" />' +
-        '</svg></button>',
+    enquire.register("screen and (max-width:479px)", {
+        unmatch : function() {
+            window.slicks.clients();
+            window.slicks.partners();
+        }
+    }).register("screen and (min-width:1200px)", {
+        unmatch : function() {
+            window.slicks.partners();
+        }
     });
 })(jQuery);
